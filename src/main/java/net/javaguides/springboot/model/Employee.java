a88023f64f8d225b3fbbee68540e3813ccd0fc50
eddyquintanilla <edquintanilla@hotmail.com>
2023-03-31 17:12:10 -0400

    Revert "par aeliminsr" Para la correccion This reverts commit ce48c88a4a352aeec0eb53f70332a4bdfb820b28.

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -66,10 +66,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id 
-		+ ", firstName=" + firstName
-		 + ", lastName=" + lastName 
-		 + ", emailId=" + emailId
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
 				+ "]";
 	}
 	
